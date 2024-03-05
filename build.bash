878d0d211ba1e9c1da0926803fc0d33fa7e9de2c
SkyFi Geek <45924209+mobileskyfi@users.noreply.github.com>
2024-03-05 09:43:22 -0800

    minor typo; fix including EFI in ZIP

--- build.bash
+++ build.bash
@@ -26,13 +26,13 @@ mkdir diskfiles
 mkdir /tmp/tmpefipart/
 mount /dev/nbd0p1 /tmp/tmpmount/
 rsync -a /tmp/tmpmount/ /tmp/tmpefipart/
+mkdir diskfiles/part1
+rsync -a /tmp/tmpmount/ ./diskfiles/part1/
 umount /dev/nbd0p1
 
 mkfs -t fat /dev/nbd0p1
 mount /dev/nbd0p1 /tmp/tmpmount/
 rsync -a /tmp/tmpefipart/ /tmp/tmpmount/
-mkdir diskfiles/part1
-rsync -a /tmp/tmpefipart/ ./diskfiles/part1/
 umount /dev/nbd0p1
 
 mount /dev/nbd0p2 /tmp/tmpmount/
