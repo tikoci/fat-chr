baf0477cbb8c36d24146fe697de2c7ee624f20c4
SkyFi Geek <45924209+mobileskyfi@users.noreply.github.com>
2024-09-23 07:16:18 -0700

    try no gdisk...

--- build.bash
+++ build.bash
@@ -43,23 +43,23 @@ umount /dev/nbd0p2
 rm -rf /tmp/tmp*
 
 # @kriszos approach
-(
-echo 2 # use GPT
-echo t # change partition code
-echo 1 # select first partition
-echo 8300 # change code to Linux filesystem 8300
-echo r # Recovery/transformation
-echo h # Hybrid MBR
-echo 1 2 # partitions added to the hybrid MBR
-echo n # Place EFI GPT (0xEE) partition first in MBR (good for GRUB)? (Y/N)
-echo   # Enter an MBR hex code (default 83)
-echo y # Set the bootable flag? (Y/N)
-echo   # Enter an MBR hex code (default 83)
-echo n # Set the bootable flag? (Y/N)
-echo n # Unused partition space(s) found. Use one to protect more partitions? (Y/N)
-echo w # write changes to disk
-echo y # confirm
-) | gdisk /dev/nbd0
+# (
+# echo 2 # use GPT
+# echo t # change partition code
+# echo 1 # select first partition
+# echo 8300 # change code to Linux filesystem 8300
+# echo r # Recovery/transformation
+# echo h # Hybrid MBR
+# echo 1 2 # partitions added to the hybrid MBR
+# echo n # Place EFI GPT (0xEE) partition first in MBR (good for GRUB)? (Y/N)
+# echo   # Enter an MBR hex code (default 83)
+# echo y # Set the bootable flag? (Y/N)
+# echo   # Enter an MBR hex code (default 83)
+# echo n # Set the bootable flag? (Y/N)
+# echo n # Unused partition space(s) found. Use one to protect more partitions? (Y/N)
+# echo w # write changes to disk
+# echo y # confirm
+# ) | gdisk /dev/nbd0
 
 # @jaclaz
 # (
